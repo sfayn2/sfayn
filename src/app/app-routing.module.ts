a3c700a7d4281b389b8b983c4867c03b2482299a
jose <nino.capati@gmail.com>
2021-05-26 21:36:20 +0800

    (1) Fixed the following issue when navigation to cart/list back & forth  - ERROR Error: Uncaught (in promise): SecurityError: Failed to execute 'pushState' on 'History': A history state object with URL  - core.js:6210 ERROR Error: Uncaught (in promise): Error: Cannot activate an already activated outlet
    (2) Fixed Products detail nav to different color variant
    (3) Set products list as landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: "/products/list",
+    pathMatch: 'full'
+},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
