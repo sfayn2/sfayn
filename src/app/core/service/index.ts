eebe8b6f997f4f89edaac5faa67d59ffd710953b
jose <nino.capati@gmail.com>
2021-06-05 13:21:57 +0800

    removed unused service

--- src/app/core/service/index.ts
+++ src/app/core/service/index.ts
@@ -1,6 +1,5 @@
 export * from './auth.service';
 export * from './product.service';
-export * from './products-graphql.service';
 export * from './site.service';
 export * from './makevar.service'
 export * from './cart.service'
