93a3985c113422581a97a3571a519a1b8e37568e
jose <nino.capati@gmail.com>
2021-06-13 18:28:31 +0800

    delete cart aligned w. backend gql

--- src/app/core/graphql/fragments.ts
+++ src/app/core/graphql/fragments.ts
@@ -352,12 +352,12 @@ export const ADD_CART = gql`
 // need to provide the __typename & id to use it for cache.evict
 export const DELETE_CART = gql`
   mutation DeleteCart($user: ID!, $sku: ID!) {
-    shoppingCart(user: $user, product: $sku, mode: 2) {
+    shopcart(user: $user, sku: $sku, mode: 2) {
       ok
-      shoppingCart {
+      shopcart {
         id
-        __typename
-      }
+        totalPrice
+      }    
     }
   }
 `;
