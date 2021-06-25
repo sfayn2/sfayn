4609934c342d9a2ffd9fa8e9c8df5c44aacb89e4
jose <nino.capati@gmail.com>
2021-06-25 18:09:03 +0800

    set placeOrder total Payment

--- src/app/checkout/checkout/checkout-placeorder/checkout-placeorder.component.ts
+++ src/app/checkout/checkout/checkout-placeorder/checkout-placeorder.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import {
+  CartService
+} from '@/core/service';
 
 @Component({
   selector: 'app-checkout-placeorder',
   templateUrl: './checkout-placeorder.component.html',
   styleUrls: ['./checkout-placeorder.component.scss']
 })
-export class CheckoutPlaceorderComponent implements OnInit {
+export class CheckoutPlaceorderComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  subscription: Subscription;
+  totalAmount: number = 0;
+
+  constructor(
+    private cartService: CartService,
+  ) { }
 
   ngOnInit(): void {
+    this.subscription = this.cartService.obj$.subscribe(
+      res => this.totalAmount = res.totalAmount 
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
